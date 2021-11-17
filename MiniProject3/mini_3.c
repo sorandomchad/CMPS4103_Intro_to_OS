8140ba07860dbd4fdfaf27840c247363990a2e06
sorandomchad <chaddon1999@gmail.com>
2021-11-17 15:28:02 -0600

    Fix mini_3.c.

--- MiniProject3/mini_3.c
+++ MiniProject3/mini_3.c
@@ -8,7 +8,7 @@
 
 int main()
 {
-    FILE *fPtr = fopen("test.txt", "r");
+    FILE *fPtr = fopen("spice.txt", "r");
     int pages[12];                  // implementation of TLB of 12 entries as an array
     unsigned int code, address, page;
     int misses = 0;
@@ -20,42 +20,42 @@ int main()
     {
 
         page = address >> 12;
-        // // the offset is 12 bits (since page size is 2^12 B) and the virtual addresss
-        // // is 32 bits, so the page number is the leftmost 20 bits.
-        // // Therefore, shifting the last 12 bits to the right gives the page number.
+        // the offset is 12 bits (since page size is 2^12 B) and the virtual addresss
+        // is 32 bits, so the page number is the leftmost 20 bits.
+        // Therefore, shifting the last 12 bits to the right gives the page number.
 
-        // for(int i = 0; i < 12; i++)
-        // {// iterating through TLB
-        //     if(pages[i] == page)
-        //     {// page found in TLB
-        //         break;                
-        //     }
-        //     else if(pages[i] == -1)
-        //     {// empty TLB entry found, TLB miss
-        //         pages[i] = page;
-        //         misses += 1;
-        //         // cout << hex << page;
-        //         break;
-        //     }
+        for(int i = 0; i < 12; i++)
+        {// iterating through TLB
+            if(pages[i] == page)
+            {// page found in TLB
+                break;                
+            }
+            else if(pages[i] == -1)
+            {// empty TLB entry found, TLB miss
+                pages[i] = page;
+                misses += 1;
+                // cout << hex << page;
+                break;
+            }
             
-        //     if(i == 11)
-        //     {// page is not in TLB, TLB miss
-        //         misses += 1;
-        //         for(int j = 0; j < 11; j++)
-        //             pages[j] = pages[j+1];
-        //             // first page brought to TLB occupuies position 0.
-        //             // FIFO algorithm consequently removes position 0 by
-        //             // copying the next element in the array to the current element
-        //             // i.e. moving the queue.
+            if(i == 11)
+            {// page is not in TLB, TLB miss
+                misses += 1;
+                for(int j = 0; j < 11; j++)
+                    pages[j] = pages[j+1];
+                    // first page brought to TLB occupuies position 0.
+                    // FIFO algorithm consequently removes position 0 by
+                    // copying the next element in the array to the current element
+                    // i.e. moving the queue.
 
-        //         pages[11] = page;       // add page to the end of the array (back of the queue)
+                pages[11] = page;       // add page to the end of the array (back of the queue)
 
-        //     } 
-        // }     
-        printf("%d %d\n", &code, &page);
+            } 
+        }     
+        // printf("%d %x\n", code, page);
     }
 
-    printf("TLB misses: %d", &misses);
+    printf("TLB misses: %d", misses);
 
     fclose(fPtr);
 
