daa8aeadefc055ede0bd8585da6b4fddd02a8863
sorandomchad <chaddon1999@gmail.com>
2021-11-30 18:26:26 -0600

    Complete project

--- MiniProject4/mini_4.cpp
+++ MiniProject4/mini_4.cpp
@@ -1,49 +1,75 @@
+/*  Chad Callender
+    CMPS 4103
+    Mini Project #4
+*/
+// To compile use g++ mini_4.cpp
+
 #include<iostream>
+#include<random>
 
 using namespace std;
 
 int main()
 {
-    int skipped = 0;
-    int average = 0;
-    int bit = 0;
-    int count = 0;
+    int skipped = 0;                    // counts skipped holes for each experiment
+    int total = 0;                      // counts sum of all skipped holes
+    float average = 0;                    // average of skipped holes for 10 trials
+    int bit = 0;                        // bit of the stored number
+    int count = 0;                      // keeps track of the number of zeros
+
+    mt19937 rng;
+    uint32_t seed_val;
+    rng.seed(seed_val);
+    uniform_int_distribution<uint32_t> uint_dist10(70000,2000000); // distribution in range [1, 6]
 
     union
-    {
+    { // x and a share the same address space
         int x;
         unsigned char a[4];
     }z;
-    z.x = 242099;
 
-    cout << hex << (int)z.a[0] << endl;
+    // cout << hex << (int)z.a[0] << endl;
 
-    for(int j = 0; j < 4; j++)
-    {
-        for(int i = 7; i >= 0; i--)
+    for(int k = 0; k < 10; k++)
+    { // loop 10 times for 10 trials
+        skipped = 0;                // resets skipped hole counter
+        z.x = uint_dist10(rng);     // generates random number
+        cout << z.x << endl;
+
+        for(int j = 0; j < 4; j++)
         {
-            bit = z.a[j] >> i;
-            bit = bit & 1;
-            cout << bit;
+            for(int i = 7; i >= 0; i--)
+            {
+                bit = z.a[j] >> i;              // shifts bit until it is LSB
+                bit = bit & 1;                  // extracts LSB
+                cout << bit;
 
-            if(bit == 0)
-                count++;
-            else
-            {
-                count = 0;
-            }
-            
-            if(count == 4)
-            {
-                cout << "\nSkipped holes: " << skipped << endl;
-                j = 4;
-                break;
+                if(bit == 0)
+                    count++;                    // counts zeros
+                else // bit is 1
+                {
+                    if(count != 0)
+                    { // there is a switch from a 0 to 1
+                        count = 0;
+                        skipped++;
+                        total++;
+                    }
+
+                }
+                
+                if(count == 4)
+                { // yay! we have space for a file of 4 blocks
+                    cout << "\nSkipped holes: " << skipped << "\n\n";
+                    j = 4;
+                    break;
+                }
             }
         }
     }
 
-    // average = skipped/10.0;
-    // cout << "Average number of holes skipped (for 10 trials): " << average;
+    cout << "Total skips: " << total << endl;
+    average = total/10.0;
+    cout << "Average number of holes skipped (for 10 trials): " << average;
 
     return 0;
 }
